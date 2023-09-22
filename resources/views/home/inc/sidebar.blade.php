6d864661d59a9cbfff1b95beaf5d621475c47afc
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2023-09-23 01:04:39 +0600

    project uploaded

---
+++ resources/views/home/inc/sidebar.blade.php
@@ -0,0 +1,35 @@
+<div class="elisc_tm_sidebar">
+    <div class="sidebar_inner">
+        <div class="author">
+            <div class="image">
+                <img src="{{asset('frontEnd')}}/img/thumbs/1-1.jpg" alt />
+                <div class="main" data-img-url="{{asset('frontEnd')}}/img/about/1.jpg"></div>
+            </div>
+            <div class="name">
+                <h3><span>Robert Elisc<span class="back">Robert Elisc</span></span></h3>
+            </div>
+        </div>
+        <div class="menu scrollable">
+            <ul class="anchor_nav">
+                <li class="current"><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#service">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#news">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+        <div class="copyright">
+            <div class="social">
+                <ul>
+                    <li><a href="#"><i class="icon-facebook-1"></i></a></li>
+                    <li><a href="#"><i class="icon-twitter-1"></i></a></li>
+                    <li><a href="#"><i class="icon-linkedin-1"></i></a></li>
+                </ul>
+            </div>
+            <div class="text">
+                <p>Copyright © 2023 Robert Elisc. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</div>
